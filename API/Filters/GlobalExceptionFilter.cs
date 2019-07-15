12deae8787956c04c61d81ce96388f744a7ed4bd
Hardys <Hardys1983@gmail.com>
2019-07-15 15:55:25 -0400

    Add logging

--- API/Filters/GlobalExceptionFilter.cs
+++ API/Filters/GlobalExceptionFilter.cs
@@ -2,19 +2,24 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 using System.Text;
 
 namespace API
 {
     public class GlobalExceptionFilter: IExceptionFilter
     {
-        public GlobalExceptionFilter()
-        {
+        private readonly ILogger _logger;
 
+        public GlobalExceptionFilter(ILogger<GlobalExceptionFilter> logger)
+        {
+            _logger = logger;
         }
 
         public void OnException(ExceptionContext context)
         {
+            _logger.LogError(context.Exception, "Catched GlobalExceptionFilter.onException");
+
             var exceptionMessage = new StringBuilder($"Exception Message: {context.Exception.Message}");
 
             for(var inner = context.Exception.InnerException; inner != null; inner = inner.InnerException)
